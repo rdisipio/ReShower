d594209b42c04d4f8cfe25d044cf95b16b7cdb81
Riccardo Di Sipio <riccardo.di.sipio@cern.ch>
2019-05-22 09:35:23 -0400

    init import

--- Makefile
+++ Makefile
@@ -1,13 +1,17 @@
-PYTHIA_CFLAGS=$(pythia8-config --cflags)
-PYTHIA_LIBS=$(pythia8-config --libs)
+PYTHIA_CFLAGS = $(shell pythia8-config --cflags)
+PYTHIA_LIBS = $(shell pythia8-config --libs)
 
-ROOT_CFLAGS=$(root-config --cflags)
-ROOT_LIBS=$(root-config --libs)
+FASTJET_LIBS = $(shell fastjet-config --libs)
+
+ROOT_CFLAGS = $(shell root-config --cflags)
+ROOT_LIBS = $(shell root-config --libs)
 
 CXX_COMMON=-O2  -pedantic -W -Wall -Wshadow -fPIC
 
-reshower : reshower.cc
-	$(CXX) $(ROOT_CFLAGS) $(ROOT_LIBS) $(PYTHIA_CFLAGS) $(PYTHIA_LIBS)  $< -o $@ $(CXX_COMMON)
+reshower : reshower.cc $(shell pythia8-config --libdir)/libpythia8.a
+	$(CXX) $< -o $@ $(CXX_COMMON) $(PYTHIA_CFLAGS) $(PYTHIA_LIBS) $(ROOT_LIBS) $(FASTJET_LIBS)
 
+clean:
+	@rm -f reshower *.o
 
 
--- reshower.cc
+++ reshower.cc
@@ -10,13 +10,13 @@ using namespace Pythia8;
 //==========================================================================
 
 void fillParticle(int id, double pT, double eta, double phi, 
-  Event& event, ParticleData& pdt, Rndm& rndm) {
+  Event& event, ParticleData& pdt) {
 
   event.reset();
 
   // E^2 - P^2 = m^2
   
-  double m = pdt.mass(id);
+  double m = pdt.m0(id);
   
   double px = pT * cos(phi);
   double py = pT * sin(phi);
@@ -25,8 +25,8 @@ void fillParticle(int id, double pT, double eta, double phi,
   double E = sqrtpos( P*P + m*m );
 
   int status = 23; // http://home.thep.lu.se/~torbjorn/pythia81php/ParticleProperties.php?filepath=files/
-  int col = 0;
-  int acol = 0;
+  int col = 101;
+  int acol = 102;
   
   // int Event::append(int id, int status, int col, int acol, double px, double py, double pz, double e, double m = 0., double scale = 0., double pol = 9.)   
   event.append( id, status, col, acol, px, py, pz, E, m );
@@ -38,13 +38,19 @@ void fillParticle(int id, double pT, double eta, double phi,
 
 int main() {
   
-  int id = 6; // top quark
+  //int id = 6; // top quark
+  int id = 25; // higgs
   double pT = 350.;
   double eta = 0.5; 
   double phi =  0.0;
 
   int nEvent = 10000;
   int nList = 3;
+
+  double pT_min = 0.;
+  if((id==6)||(id==-6)) pT_min = 350.;
+  if(id==23) pT_min = 200.;
+  if(id==25) pT_min = 250.;
 
   // Generator; shorthand for event and particleData.
   Pythia pythia;  
@@ -55,7 +61,7 @@ int main() {
   pythia.readString("ProcessLevel:all = off");
 
   // Also allow resonance decays, with showers in them
-  pythia.readString("Standalone:allowResDec = on");
+  //pythia.readString("Standalone:allowResDec = on");
 
   // Optionally switch off decays.
   //pythia.readString("HadronLevel:Decay = off");
@@ -65,35 +71,82 @@ int main() {
   pythia.readString("Next:numberShowProcess = 0");
   pythia.readString("Next:numberShowEvent = 0");
 
-  // top/H hadronic decays only
-  pythia.particleData.readString("6:2::products = 5 -24"); // t
-  pythia.particleData.readString("-6:2::products = -5 24"); // tbar
-  pythia.particleData.readString("23:2::products = 5 -5"); // Z0
-  pythia.particleData.readString("25:2::products = 5 -5"); // H0
+  // top/Z/H hadronic decays only
+  pythia.readString("24:onMode = off");
+  pythia.readString("24:onIfAny = 1 2 3 4 -1 -2 -3 -4");
+
+  pythia.readString("23:onMode = off");
+  pythia.readString("23:onIfAny = 5 -5");
+  
+  pythia.readString("25:onMode = off");
+  pythia.readString("25:onIfAny = 5 -5");
   
   // Initialize.
   pythia.init();
+
+  // Setup fastjet
+  // Fastjet analysis - select algorithm and parameters
+  double Rparam = 1.0;
+  fastjet::Strategy               strategy = fastjet::Best;
+  fastjet::RecombinationScheme    recombScheme = fastjet::E_scheme;
+  fastjet::JetDefinition         *jetDef = NULL;
+  jetDef = new fastjet::JetDefinition(fastjet::kt_algorithm, Rparam,
+                                      recombScheme, strategy);
+
+  // Fastjet input
+  std::vector <fastjet::PseudoJet> fjInputs;
 
   // Book histograms
 
   // Begin of event loop.
   for (int iEvent = 0; iEvent < nEvent; ++iEvent) {
-    fillParticle( id, pT, eta, phi, event, pdt, pythia.rndm );
+    fillParticle( id, pT, eta, phi, event, pdt );
 
     // Generate events. Quit if failure.
     if (!pythia.next()) {
       cout << " Event generation aborted prematurely, owing to error!\n"; 
       break;
     }
-  }
 
-  // List first few events.
-  if (iEvent < nList) { 
-    event.list();
-    // Also list junctions.
-    event.listJunctions();
-  }
+    // List first few events.
+    if (iEvent < nList) { 
+     event.list();
+     // Also list junctions.
+     event.listJunctions();
+    }
 
+    // reset fastjet
+    fjInputs.resize(0);
+    
+    for (int i = 0; i < pythia.event.size(); ++i) {
+      if (!pythia.event[i].isFinal())        continue;
+
+      // No neutrinos
+      if (pythia.event[i].idAbs() == 12 || pythia.event[i].idAbs() == 14 ||
+          pythia.event[i].idAbs() == 16)     continue;
+
+      // Only |eta| < 2.5
+      if (fabs(pythia.event[i].eta()) > 2.5) continue;
+
+      fjInputs.push_back( fastjet::PseudoJet( pythia.event[i].px(),
+        pythia.event[i].py(), pythia.event[i].pz(), pythia.event[i].e() ) );
+    } // end loop over particles
+
+    if (fjInputs.size() == 0) {
+      cout << "Error: event with no final state particles" << endl;
+      continue;
+    }
+
+    // Run Fastjet algorithm
+    vector <fastjet::PseudoJet> all_jets;
+    fastjet::ClusterSequence clustSeq(fjInputs, *jetDef);
+
+    // Extract inclusive jets sorted by pT
+    all_jets = sorted_by_pt( clustSeq.inclusive_jets(pT_min) );
+   
+  }
   
-  
+  pythia.stat();
+
+  return 0;
 }
